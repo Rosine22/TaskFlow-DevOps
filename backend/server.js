73c40f7a6fa73f110ef6c73c19a0767ccbdc5193
Rosine22 <r.uwineza@alustudent.com>
2025-08-07 13:55:30 -0600

    Add new updates

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import logger from './utils/logger';
 
-logger.info('Server started successfully');
+logger.info('Server started successfully now ');
 logger.error('An error occurred during startup');
 
 
